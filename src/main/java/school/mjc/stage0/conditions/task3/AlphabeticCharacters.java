5df575bae350932b4ad79495bb2149a32ecaedd6
wilkat <katarzyna.f.wilk@gmail.com>
2022-11-03 22:29:24 +0100

    new

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.task3;
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
         String nowy = String.valueOf(character);
-        if(nowy.equals("[a-zA-Z]")) {
+        if(nowy.equals("^[a-zA-Z]")) {
             System.out.println("wrong alphabet!");
         } else if (nowy.equals("[aeiouy")) {
             System.out.println("Vowel");
--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,8 +2,9 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
-        if (month > 0 || month <= 12) {
-            if (month <= 2 || month == 12) {
+            if (month < 0 || month > 12) {
+                System.out.println("wrong number!");
+            } else if (month <= 2 || month == 12) {
                 System.out.println("Winter");
             } else if (month >= 3 && month <= 5) {
                 System.out.println("Spring");
@@ -12,9 +13,8 @@ public class Seasons {
             } else {
                 System.out.println("Autumn");
             }
-        } else {
-            System.out.println("wrong number!");
+
         }
 
     }
-}
+
