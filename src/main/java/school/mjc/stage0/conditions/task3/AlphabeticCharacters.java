9452a8fee5a14f87eaf2af47f1220731772caa05
wilkat <katarzyna.f.wilk@gmail.com>
2022-11-04 18:42:13 +0100

    new

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -3,9 +3,9 @@ package school.mjc.stage0.conditions.task3;
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
         String nowy = Character.toString(character);
-        if(nowy.equals("^[a-zA-Z]")) {
+        if(nowy.matches("^[a-zA-Z]")) {
             System.out.println("wrong alphabet!");
-        } else if (nowy.equals("[aeiouy")) {
+        } else if (nowy.matches("[aeiouy]")) {
             System.out.println("Vowel");
         } else {
             System.out.println("Consonant");
--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -4,7 +4,7 @@ public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
 
         String nowy = Character.toString(symbol);
-        if (nowy.equals("[a-zA-Z]")) {
+        if (nowy.matches("[a-zA-Z]")) {
             System.out.println("English");
         } else {
             System.out.println("Non English");
