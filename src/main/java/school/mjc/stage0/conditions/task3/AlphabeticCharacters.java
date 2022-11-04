5b3f75e5dec3987cde51f8c97e45620aa072b424
wilkat <katarzyna.f.wilk@gmail.com>
2022-11-04 19:14:37 +0100

    new

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.task3;
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
         String nowy = Character.toString(character);
-        if(nowy.matches("^[a-zA-Z]")) {
+        if(nowy.matches("[^a-zA-Z]")) {
             System.out.println("wrong alphabet!");
         } else if (nowy.matches("[aeiouy]")) {
             System.out.println("Vowel");
